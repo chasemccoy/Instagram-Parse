1c47faca6e17906c5d83ae0937ce4b8da1da6fce
Chase McCoy <chasem000@gmail.com>
2016-03-01 17:32:18 -0600

    Pre-work

---
+++ Instagram/Instagram/LoginViewController.swift
@@ -0,0 +1,60 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by Chase McCoy on 3/1/16.
+//  Copyright © 2016 Chase McCoy. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+  @IBOutlet var usernameField: UITextField!
+  @IBOutlet var passwordField: UITextField!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  @IBAction func onSignIn(sender: UIButton) {
+    let username = usernameField.text ?? ""
+    let password = passwordField.text ?? ""
+    
+    PFUser.logInWithUsernameInBackground(username, password: password) { (user: PFUser?, error: NSError?) -> Void in
+      if let error = error {
+        print("User login failed.")
+        print(error.localizedDescription)
+      } else {
+        print("User logged in successfully")
+        // display view controller that needs to shown after successful login
+        self.performSegueWithIdentifier("loginSegue", sender: nil)
+      }
+    }
+  }
+  
+  @IBAction func onSignUp(sender: UIButton) {
+    let newUser = PFUser()
+    
+    // set user properties
+    newUser.username = usernameField.text
+    newUser.password = passwordField.text
+    
+    // call sign up function on the object
+    newUser.signUpInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+      if let error = error {
+        print(error.localizedDescription)
+        if error.code == 202 {
+          print("Username is taken")
+        }
+      } else {
+        print("User Registered successfully")
+        // manually segue to logged in view
+      }
+    }
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+}
